cdca6e3da86a7a0d85261168d94e508d1cd161db
Raghul-M <ragm@redhat.com>
2024-06-28 15:18:25 +0530

    changed webpage design

--- app.py
+++ app.py
@@ -4,7 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World Raghul"
+    return '''
+    <html>
+        <head>
+            <style>
+                h1,h3 {
+                    color: #4F6F52;  /* Change to your desired color */
+                    text-align:center;
+                    font-size:30px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1> Python Webapp Deployment on Heroku Using Github Actions </h1>
+            <h3 style="color:black"> Visit here for Documentation : <a href="https://github.com/Raghul-M/Python-Github_Actions-Heroku">https://github.com/Raghul-M/Python-Github_Actions-Heroku</a></h3>
+        </body>
+    </html>
+    '''
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
 from app import index
 
 def test_index():
-    assert index() == "Hello World Raghul"
+    assert "Python Webapp Deployment on Heroku Using Github Actions" in index()
